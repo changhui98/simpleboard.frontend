7faffc14859de2579241e85d9b7cf41b6b793e09
changhui98 <net_0102@naver.com>
2026-01-19 20:54:59 +0900

    Feat : 서버로 부터 게시글 데이터 받아오기 기능 구현

--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,77 @@
-import {useState} from "react";
-import {Link} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./css/list.css";
+import axios from "axios";
 
 let Home = () => {
-
-  const allPosts = Array.from({length: 25}, (_, index) => ({
-    id: index + 1,
-    title: `${index + 1} 번째 게시글`,
-    content: `${index+1} 번째 게시글 내용입니다.`
-  }));
-
-  const postsPerPage = 10;
+  const [posts, setPosts] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPage = indexOfLastPost - postsPerPage;
-  const currentPosts = allPosts.slice(indexOfFirstPage, indexOfLastPost);
+  const getPostList = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/posts`, {
+        params: {
+          page: currentPage - 1,
+          size: postsPerPage,
+        },
+      })
+      .then((res) => {
+        setPosts(res.data.content);
+        setTotalPages(res.data.totalPages);
+      })
+      .catch((err) => {
+        console.err("게시글 가져오기 실패");
+      });
+  };
 
-  const totalPages = Math.ceil(allPosts.length / postsPerPage);
-  const pageNumbers = Array.from({length: totalPages}, (_, index) => index +1);
+  useEffect(() => {
+    getPostList();
+  }, [currentPage]);
+
+  const pageNumbers = Array.from(
+    { length: totalPages },
+    (_, index) => index + 1,
+  );
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
-  }
+  };
 
   return (
-      <div className={"home-container"}>
-        <h1 className={"home-title"}>게시글 목록</h1>
-        <div className={"post-list"}>
-          {
-            currentPosts.map(post=>(
-                <div key={post.id} className={"post-card"}>
-                  <h2 className={"post-title"}>
-                    <Link to={`/post/${post.id}`}>{post.title}</Link>
-                  </h2>
-                  <p className={"post-content"}>
-                    {post.content}
-                  </p>
-                </div>
-            ))
-          }
-        </div>
-
-        <div className={"pagination"}>
-          {
-            pageNumbers.map(number => (
-                <button key={number} className={`page-btn ${number===currentPage ? 'active' : ''}`} onClick={() => {
-                  handlePageChange(number);
-                }}>
-                  {number}
-                </button>
-            ))
-          }
-        </div>
-
-        <Link to={"/create"} className={"create-link"}>게시글 작성하기</Link>
+    <div className={"home-container"}>
+      <h1 className={"home-title"}>게시글 목록</h1>
+      <div className={"post-list"}>
+        {posts.map((post) => (
+          <div key={post.id} className={"post-card"}>
+            <h2 className={"post-title"}>
+              <Link to={`/post/${post.id}`}>{post.title}</Link>
+            </h2>
+            <p className={"post-content"}>{post.content}</p>
+          </div>
+        ))}
       </div>
+
+      <div className={"pagination"}>
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            className={`page-btn ${number === currentPage ? "active" : ""}`}
+            onClick={() => {
+              handlePageChange(number);
+            }}
+          >
+            {number}
+          </button>
+        ))}
+      </div>
+
+      <Link to={"/create"} className={"create-link"}>
+        게시글 작성하기
+      </Link>
+    </div>
   );
-}
+};
 
 export default Home;
