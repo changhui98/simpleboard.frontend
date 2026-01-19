6b0b2bfcf6a6a51ad40c8e295536c0dd232eabd4
changhui98 <net_0102@naver.com>
2026-01-19 20:39:53 +0900

    Feat : 스프링 서버로 데이터 연동

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,22 +1,64 @@
+import axios from "axios";
 import "./css/create.css";
+import { useState } from "react";
 
 let CreatePost = () => {
+  let [formData, setFormData] = useState({
+    title: "",
+    content: "",
+  });
+
+  let onChangeFormData = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onClickSubmit = () => {
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/posts`, formData)
+      .then((res) => {
+        console.log("게시글 등록 성공", res.data);
+      })
+      .catch((err) => {
+        console.log("게시글 등록 실패 :", err);
+      });
+  };
+
   return (
-      <div className={"create-post-container"}>
-        <h1 className={"create-post-title"}>게시글 작성</h1>
-        <form className={"create-post-form"}>
-          <div className={"form-group"}>
-            <label htmlFor={"title"}>제목</label>
-            <input id={"title"} type={"text"} placeholder={"제목을 입력하세요."} />
-          </div>
-          <div className={"form-group"}>
-            <label htmlFor={"content"}>내용</label>
-            <textarea id={"content"} placeholder={"내용을 입력하세요."}></textarea>
-          </div>
-          <button type={"submit"} className={"submit-button"}>등록</button>
-        </form>
-      </div>
+    <div className={"create-post-container"}>
+      <h1 className={"create-post-title"}>게시글 작성</h1>
+      <form className={"create-post-form"}>
+        <div className={"form-group"}>
+          <label htmlFor={"title"}>제목</label>
+          <input
+            name={"title"}
+            id={"title"}
+            type={"text"}
+            placeholder={"제목을 입력하세요."}
+            onChange={onChangeFormData}
+          />
+        </div>
+        <div className={"form-group"}>
+          <label htmlFor={"content"}>내용</label>
+          <textarea
+            name={"content"}
+            id={"content"}
+            placeholder={"내용을 입력하세요."}
+            onChange={onChangeFormData}
+          ></textarea>
+        </div>
+        <button
+          onClick={onClickSubmit}
+          type={"submit"}
+          className={"submit-button"}
+        >
+          등록
+        </button>
+      </form>
+    </div>
   );
-}
+};
 
 export default CreatePost;
