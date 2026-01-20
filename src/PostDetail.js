63d2e9e368e134b45e65a0d60499d18ff638b9ea
changhui98 <net_0102@naver.com>
2026-01-20 11:17:49 +0900

    Feat : 게시글 상세 조회 기능 구현

--- src/PostDetail.js
+++ src/PostDetail.js
@@ -1,30 +1,53 @@
-import {Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./css/detail.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 let PostDetail = () => {
+  const { id } = useParams();
 
-  const {id} = useParams();
-  const post = {
-    id,
-    title : `${id}번째 게시글`,
-    content : `${id}번째 게시글의 상세 내용`
-  }
+  let [post, setPost] = useState({
+    title: "",
+    content: "",
+  });
+
+  const getPost = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/posts/${id}`)
+      .then((res) => {
+        console.log("게시글 가져오기 성공 : ", res.data);
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.err("게시글 가져오기 실패 : ", err);
+      });
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
 
   const handleDelete = () => {
     alert("게시글이 삭제되었습니다.");
-  }
+  };
 
   return (
-      <div className={"post-detail-container"}>
-        <h1 className={"post-detail-title"}>{post.title}</h1>
-        <p className={"post-detail-content"}>{post.content}</p>
-        <div className={"button-group"}>
-          <Link to={`/post/edit/${id}`} className={"edit-button"}>수정하기</Link>
-          <button onClick={handleDelete} className={"delete-button"}>삭제하기</button>
-        </div>
-        <Link to={"/"} className={"back-link"}>목록으로 돌아가기</Link>
+    <div className={"post-detail-container"}>
+      <h1 className={"post-detail-title"}>{post.title}</h1>
+      <p className={"post-detail-content"}>{post.content}</p>
+      <div className={"button-group"}>
+        <Link to={`/post/edit/${id}`} className={"edit-button"}>
+          수정하기
+        </Link>
+        <button onClick={handleDelete} className={"delete-button"}>
+          삭제하기
+        </button>
       </div>
+      <Link to={"/"} className={"back-link"}>
+        목록으로 돌아가기
+      </Link>
+    </div>
   );
-}
+};
 
 export default PostDetail;
